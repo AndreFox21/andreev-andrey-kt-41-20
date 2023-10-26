8ffc059a1e9e17989db0dae47effcb158cf04294
andrey <andrey.andreev.2002@gmail.com>
2023-10-26 11:55:00 +0300

    Fix bugs related to folder hierarchy. Finally added tests.

---
+++ AndreevAndreyKT-41-20/Models/AcademGroup.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AndreevAndreyKT_41_20.Models
+{
+    public class AcademGroup
+    {
+        public int AcademGroupId { get; set; }
+        public string GroupName { get; set; }
+        public string GroupDescription { get; set; }
+
+        public bool IsValidGroupName() // Проверка формата ввода названия группы
+        {
+            return Regex.Match(GroupName, @"\D*-\d*-\d\d").Success;
+        }
+
+    }
+}
