92bbc68638df96b5ab8794ccb6aab6037faff90d
AndreFox21 <108476914+AndreFox21@users.noreply.github.com>
2023-11-02 20:04:58 +0300

    Update Program.cs

--- AndreevAndreyKT-41-20/Program.cs
+++ AndreevAndreyKT-41-20/Program.cs
@@ -18,13 +18,12 @@ try
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
 
-    // прописываем контекст
+    // ГЇГ°Г®ГЇГЁГ±Г»ГўГ ГҐГ¬ ГЄГ®Г­ГІГҐГЄГ±ГІ
     builder.Services.AddDbContext<StudDatabaseContext>(options =>
         options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
     builder.Services.AddServices();
-
-
+    
     var app = builder.Build();
 
     // Configure the HTTP request pipeline.
